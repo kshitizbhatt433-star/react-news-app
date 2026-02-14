7f4acf664b52c09302416b0bf578e6774a56d9ff
Kshitiz Bhatt <kshitizbhatt433@gmail.com>
2026-02-14 14:59:21 +0530

    Updated background and loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,9 +4,9 @@ const Loader = () => {
 	return (
 		<div className="loader-wrap">
 			<div className="loader">
-				<div className="dot dot-1">📰</div>
-				<div className="dot dot-2">✨</div>
-				<div className="dot dot-3">🔎</div>
+				<div className="dot dot-1">.</div>
+				<div className="dot dot-2">.</div>
+				<div className="dot dot-3">.</div>
 			</div>
 			<p className="loader-text">Fetching the latest stories for you…</p>
 		</div>
